a23d8157f9ea72284eb55b74e3ce767ddb58c134
Sullivan LACHETEAU <lacheteau.s@gmail.com>
2020-03-15 01:01:05 +0100

    Entry point override

---
+++ Hermes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hermes
+{
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			var app = new App();
+			var window = new MainWindow();
+
+			app.Run(window);
+		}
+	}
+}
