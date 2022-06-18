24273c832188f7faad5ac51b6865eb5a931a64f0
Sullivan LACHETEAU <lacheteau.s@gmail.com>
2022-06-18 00:36:52 -0400

    blabla

---
+++ Hermes/Resources/Commands/MaximizeCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Hermes.Resources.Commands
+{
+	public class MaximizeCommand : ICommand
+	{
+		public event EventHandler CanExecuteChanged;
+
+		public bool CanExecute(object parameter) => true;
+
+		public void Execute(object parameter)
+		{
+			var window = parameter as Window;
+
+			if (window != null)
+				window.WindowState = (window.WindowState == WindowState.Maximized) ? WindowState.Normal : WindowState.Maximized;
+		}
+	}
+}
---
+++ WpfApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			try
+			{
+				var app = new App();
+				var window = new MainWindow();
+
+				app.Run(window);
+			}
+			catch (Exception e)
+			{
+
+			}
+		}
+	}
+}
